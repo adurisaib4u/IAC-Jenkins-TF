ab8c606cc87236cae556f0261bd928268255613b
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-14 00:06:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "JenkinsTerraform"
-  location = "East US"
+# Reference the existing resource group
+data "azurerm_resource_group" "existing" {
+  name = var.existing_resource_group_name
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "TerraformWindowsServicePlan"
+  name                = var.azurerm_service_plan
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku_name            = "P1v2"
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "example" {
 }
 
 resource "azurerm_windows_web_app" "example" {
-  name                = "TerraformWindowsWebapp"
+  name                = var.azurerm_windows_web_app
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
