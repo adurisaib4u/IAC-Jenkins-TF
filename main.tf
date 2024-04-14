148bb6ea46fdfc83cdbfd9b946f7f271bba1f30f
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-13 23:06:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,20 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = JenkinsTerraform
-  location = East US
+  name     = "JenkinsTerraform"
+  location = "East US"
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = TerraformWindowsServicePlan
+  name                = "TerraformWindowsServicePlan"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  sku_name            = P1v2
-  os_type             = Windows
+  sku_name            = "P1v2"
+  os_type             = "Windows"
 }
 
 resource "azurerm_windows_web_app" "example" {
-  name                = TerraformWindowsWebapp
+  name                = "TerraformWindowsWebapp"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
