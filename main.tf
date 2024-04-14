9036ab2917bded3a26a396962c953d778a75500f
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-13 23:05:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,23 @@ provider "azurerm" {
   features {}
 }
 
-# Reference the existing resource group
-data "azurerm_resource_group" "existing" {
-  name = var.existing_resource_group_name
+resource "azurerm_resource_group" "example" {
+  name     = JenkinsTerraform
+  location = East US
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = var.azurerm_service_plan
+  name                = TerraformWindowsServicePlan
   resource_group_name = azurerm_resource_group.example.name
-  location            = East US
-  sku_name            = "P1v2"
-  os_type             = "Windows"
+  location            = azurerm_resource_group.example.location
+  sku_name            = P1v2
+  os_type             = Windows
 }
 
 resource "azurerm_windows_web_app" "example" {
-  name                = var.azurerm_windows_web_app
+  name                = TerraformWindowsWebapp
   resource_group_name = azurerm_resource_group.example.name
-  location            = East US
+  location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
 
   site_config {}
