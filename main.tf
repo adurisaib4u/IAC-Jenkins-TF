3db3376d5f01a97e5146b2c090ff5ed80d3a80bc
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-13 22:18:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "azurerm_resource_group" "existing" {
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example"
+  name                = "var.azurerm_service_plan"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   sku_name            = "P1v2"
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "example" {
 }
 
 resource "azurerm_windows_web_app" "example" {
-  name                = "example"
+  name                = "var.azurerm_windows_web_app"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
