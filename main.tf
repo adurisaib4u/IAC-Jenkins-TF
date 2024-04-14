202e87417f3d9cdc8c89cfb55e881da514b9f355
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-13 22:16:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,35 +7,19 @@ data "azurerm_resource_group" "existing" {
   name = var.existing_resource_group_name
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = var.app_service_plan_name
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
-  kind                = "Windows"
-  reserved            = true
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+resource "azurerm_service_plan" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
 }
 
-resource "azurerm_app_service" "example" {
-  name                = var.app_service_name
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
-  app_service_plan_id = azurerm_app_service_plan.example.id  # Use azurerm_app_service_plan
+resource "azurerm_windows_web_app" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
 
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-  }
-}
-
-output "app_service_default_hostname" {
-  value = azurerm_app_service.example.default_site_hostname
+  site_config {}
 }
