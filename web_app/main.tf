94e7772c92d55244363ed27b2a267a5d429c2712
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-15 16:07:45 -0400

    Update main.tf

--- web_app/main.tf
+++ web_app/main.tf
@@ -3,6 +3,7 @@ data "azurerm_resource_group" "existing" {
   name = var.existing_resource_group_name
 }
 
+# Define the App Service Plan
 resource "azurerm_service_plan" "example" {
   name                = var.azurerm_service_plan
   resource_group_name = data.azurerm_resource_group.existing.name
@@ -11,11 +12,13 @@ resource "azurerm_service_plan" "example" {
   os_type             = "Windows"
 }
 
+# Define the Windows Web App
 resource "azurerm_windows_web_app" "example" {
   name                = var.azurerm_windows_web_app
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
   service_plan_id     = azurerm_service_plan.example.id
 
-  site_config {}
+  site_config {
+  }
 }
