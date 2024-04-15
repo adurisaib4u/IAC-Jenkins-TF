f743a98c925cb3569d3cd7a428705082c47c8ff2
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-15 15:02:47 -0400

    Update module.tf

--- module.tf
+++ module.tf
@@ -2,6 +2,5 @@ module "web_app" {
   source = "./web_app"
 
   existing_resource_group_name = var.existing_resource_group_name
-  azurerm_service_plan         = var.azurerm_service_plan
-  azurerm_windows_web_app      = var.azurerm_windows_web_app
+
 }
