d9a3ea6b303798455379d0c34dd4f4e110c9a460
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-15 16:14:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,6 @@ terraform {
   }
 }
 
-module "web_app" {
-  source = "./web_app"
-
-  existing_resource_group_name = var.existing_resource_group_name
-  azurerm_service_plan         = var.azurerm_service_plan
-  azurerm_windows_web_app      = var.azurerm_windows_web_app
-}
-
 output "web_app_url" {
   value = module.web_app.web_app_url
 }
