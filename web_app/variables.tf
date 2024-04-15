cbf644fea667649e53b29512c39eb99bb2ab5eec
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-15 16:08:22 -0400

    Update variables.tf

--- web_app/variables.tf
+++ web_app/variables.tf
@@ -1,14 +1,11 @@
 variable "existing_resource_group_name" {
   description = "The name of the existing resource group where the web app will be deployed."
-  default     = "IAC-Jenkins-TF"
 }
 
 variable "azurerm_service_plan" {
   description = "The name of the Basic App Service Plan."
-  default     = "Windowsappteraformplan"
 }
 
 variable "azurerm_windows_web_app" {
   description = "The name of the App Service (web app)."
-  default     = "SaiKannaTerraformWebapp"
-}    
+}
