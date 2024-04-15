b04bdc6283aea18638b943658e5759e01a5779a3
adurisaib4u <144969760+adurisaib4u@users.noreply.github.com>
2024-04-15 14:11:36 -0400

    Update main.tf

--- web_app/main.tf
+++ web_app/main.tf
@@ -1,16 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+# Reference the existing resource group
+data "azurerm_resource_group" "existing" {
+  name = var.existing_resource_group_name
+}
+
 resource "azurerm_service_plan" "example" {
   name                = var.azurerm_service_plan
-  resource_group_name = var.existing_resource_group_name
-  location            = var.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
   sku_name            = "B1"
   os_type             = "Windows"
 }
 
 resource "azurerm_windows_web_app" "example" {
   name                = var.azurerm_windows_web_app
-  resource_group_name = var.existing_resource_group_name
-  location            = var.location
-  app_service_plan_id = azurerm_service_plan.example.id
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  service_plan_id     = azurerm_service_plan.example.id
 
   site_config {}
 }
